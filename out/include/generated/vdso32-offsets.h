35daf58fb20ddd2f717c6fd0c377673f4a53174b
Jonathan Liu <net147@gmail.com>
2017-06-12 18:23:17 +1000

    UPSTREAM: scripts/mkcompile_h: Remove trailing spaces from compiler version
    Improves the output of "cat /proc/version" by getting rid of the
    trailing space at the end of the compiler version when the kernel
    is compiled using GCC.
    
    Signed-off-by: Jonathan Liu <net147@gmail.com>
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    (cherry picked from commit adcc3f7cee29eb831f79f4ac7ba6a0fbce7ac936)
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    (cherry picked from commit bfb659d3b03b5baaea0c7658cd71c605446dd4b2)
    Signed-off-by: enzaik <imernestodiaz@gmail.com>

---
+++ out/include/generated/vdso32-offsets.h
@@ -0,0 +1,4 @@
+#define vdso_offset_compat_rt_sigreturn_arm	0x0ae0
+#define vdso_offset_compat_rt_sigreturn_thumb	0x0b00
+#define vdso_offset_compat_sigreturn_arm	0x0ad0
+#define vdso_offset_compat_sigreturn_thumb	0x0af0
