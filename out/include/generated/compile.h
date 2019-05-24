11cfb082537c654f2c28c579f688036e1e8c4ebc
enzaik <imernestodiaz@gmail.com>
2019-05-24 20:25:10 +0200

    Remove out folder after .gitignore issue

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#0 SMP PREEMPT Fri May 17 12:48:04 CEST 2019"
-#define LINUX_COMPILE_BY "enzaik"
-#define LINUX_COMPILE_HOST "enzaik-wolfscave"
-#define LINUX_COMPILER "gcc version 7.3.1 20180402 (GCC)"
--- out/include/generated/vdso32-offsets.h
+++
@@ -1,4 +0,0 @@
-#define vdso_offset_compat_rt_sigreturn_arm	0x0ae0
-#define vdso_offset_compat_rt_sigreturn_thumb	0x0b00
-#define vdso_offset_compat_sigreturn_arm	0x0ad0
-#define vdso_offset_compat_sigreturn_thumb	0x0af0
